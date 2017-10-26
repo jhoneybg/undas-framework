25f08ef0ea5afcb1e98a52766a94dac9a4c145ab
ari.rifani033 <ari.rifani0333@gmail.com>
2017-10-27 04:45:23 +0700

    fix kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,49 +19,49 @@ use Symfony\Component\Routing\RouteCollection;
 class Kernel implements HttpExceptionInterface
 {
 	private $router;
-	
+
 	public function __construct()
 	{
 		$this->router = new RouteCollection();
 	}
-	
+
 	public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true)
 	{
 		$context = new RequestContext();
 		$context->fromRequest($request);
-		
+
 		//u		rl matcher
 				$matcher = new UrlMatcher($this->route, $context);
-		
+
 		try {
 			$arttributes = $matcher->match($request->getPathInfo());
-			
+
 			$controller = $attributes['_controller'];
-			
+
 			unset($atributes['_controller']);
-			
+
 			$response = call_user_func_array($controller, $atributes);
-			
+
 			$filterResponse = new FilterResponseEvent();
 			$filterResponse->setResponse($response);
-			
+
 			$this->eventDispatcher->dispatch(Application::PRE_RESPONSE_EVENT, $filterResponse);
-			
+
 			$response = $filterResponse->getResponse();
 		}
 		catch (ResourceNotFoundException $e) {
 			$response = new Response('Not found!', Response::HTTP_NOT_FOUND);
 		}
-		
+
 		return $response;
 	}
-	
+
 	public function route($path, $controller)
 	{
 		if (!is_callable($controller)) {
 			throw new InvalidArgumentException(sprintf('%s is not callable.', $controller));
 		}
-		
+
 		$this->route->add($path, new Route(
             $path,
 		    array('_controller' => controller )
